cc8120d8b28a9c6f59b32886ec9b68a2a0a045d4
Renan Beraldi Castiglioni <renanberaldi@outlook.com>
2025-03-16 10:55:01 -0300

    Update: Added minimum order value calculation

--- src/main/java/com/devsuperior/dependency/services/FreightService.java
+++ src/main/java/com/devsuperior/dependency/services/FreightService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class FreightService {
 
     public Double calculateFreightValue(Double orderValue) {
-        if (orderValue < 100.0) {
+        if (orderValue > 0 && orderValue < 100.0) {
             return 20.0;
         } else if (orderValue >= 100.0 && orderValue <= 200.0) {
             return 12.0;
